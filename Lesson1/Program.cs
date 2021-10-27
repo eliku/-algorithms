6907c403af4d3ca1583538001aee91ea432ff80e
GeekBrains <kubasovae@mail.ru>
2021-10-27 22:55:59 +0400

    Create project

---
+++ Lesson1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/* Домашнее задание
+ * Требуется реализовать на C# функцию согласно блок-схеме.
+ * Блок-схема описывает алгоритм проверки, простое число или нет.
+
+ */
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод числа
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+        }
+    }
+}
