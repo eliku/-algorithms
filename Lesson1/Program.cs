3c823ae576a3d9837f2038c3682cea4f7f14edde
GeekBrains <kubasovae@mail.ru>
2021-10-27 23:28:18 +0400

    verification code

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -8,33 +8,97 @@ namespace Lesson1
 {
     class Program
     {
-        static void Main(string[] args)
+        public class TestCase
+        {
+            public int Num { get; set; }
+            public string Expected { get; set; }
+            public Exception ExpectedException { get; set; }
+        }
+
+        static string PrimeNumber(int n)
         {
             //блок переменных
-            int number, d, i;
-            //ввод числа
-            Console.WriteLine("Введите число");
-            number = Convert.ToInt32(Console.ReadLine());
+            int d, i;
 
             d = 0;
             i = 2;
 
-            while (i < number)
+            while (i < n)
             {
-                if ((number % i) == 0) d++;
+                if ((n % i) == 0) d++;
                 i++;
             }
             if (d == 0)
             {
-                Console.WriteLine($"Число {number} простое");
+                return $"Число простое";
             }
             else
             {
-                Console.WriteLine($"Число {number} не простое");
+                return $"Число не простое";
             }
 
+        }
 
+        static void TestPrimeNumber(TestCase testCase)
+        {
+            try
+            {
 
+                var actual = PrimeNumber(testCase.Num);
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    //TODO add type exception tests;
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var testCase1 = new TestCase()
+            {
+                Num = 7,
+                Expected = $"Число простое",
+                ExpectedException = null
+
+            };
+
+            var testCase2 = new TestCase()
+            {
+                Num = 22,
+                Expected = $"Число простое",
+                ExpectedException = null
+
+            };
+
+            var testCase3 = new TestCase()
+            {
+                Num = 22,
+                Expected = $"Число не простое",
+                ExpectedException = null
+
+            };
+
+            TestPrimeNumber(testCase1);
+            TestPrimeNumber(testCase2);
+            TestPrimeNumber(testCase3);
         }
     }
 }
+
