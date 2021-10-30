776a64bdfa5584d1404fc40c195f619abd736292
GeekBrains <kubasovae@mail.ru>
2021-10-31 00:25:02 +0400

    Add1

--- Lesson2/Lesson2/Node.cs
+++ Lesson2/Lesson2/Node.cs
@@ -12,26 +12,23 @@ namespace Lesson2
         public Node NextNode { get; set; }
         public Node PrevNode { get; set; }
 
+        // возвращает количество элементов в списке
         public int GetCount()
         {
-            var node = new Node();
             int Cnt = 0;
-            while (node.NextNode != null) Cnt++;
+            while (this.NextNode != null)
+            {
+                Cnt++;
+               // this.No = node.NextNode;
+            }
             return Cnt;
         }
-
-
+        // добавляет новый элемент списка
+        public void AddNode(int value) 
+        {
+            var newNode = new Node { Value = value, PrevNode = this, NextNode = null};
+            this.NextNode = newNode;
+            this.PrevNode = this;
+        }
     }
-    //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
-    public interface ILinkedList
-    {
-        int GetCount();                          // возвращает количество элементов в списке
-     /*   void AddNode(int value);                 // добавляет новый элемент списка
-        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
-        void RemoveNode(int index);              // удаляет элемент по порядковому номеру
-        void RemoveNode(Node node);              // удаляет указанный элемент
-        Node FindNode(int searchValue);          // ищет элемент по его значению
-     */
-    }
-
 }
--- Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Program.cs
@@ -7,6 +7,11 @@ namespace Lesson2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ILinkedList newNode = new Node();
+            Console.WriteLine(newNode.GetCount());
+            newNode.AddNode(6);
+            newNode.AddNode(7);
+            Console.WriteLine(newNode.GetCount());
         }
     }
 }
