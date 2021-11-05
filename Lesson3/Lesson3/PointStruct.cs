948086cbb16f530623cae13705981919e231b387
GeekBrains <kubasovae@mail.ru>
2021-11-05 21:06:17 +0400

    Create class

---
+++ Lesson3/Lesson3/PointStruct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class PointStruct
+    {
+        public int X;
+        public int Y;
+
+        public static float PointDistance(PointStruct pointOne, PointStruct pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+    }
+}
