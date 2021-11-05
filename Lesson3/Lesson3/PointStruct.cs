405182582cb5e5bbcd5fff3a75f13ebf23fa8231
GeekBrains <kubasovae@mail.ru>
2021-11-05 22:12:35 +0400

    The end

--- Lesson3/Lesson3/PointStruct.cs
+++ Lesson3/Lesson3/PointStruct.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Lesson3
 {
@@ -11,11 +8,27 @@ namespace Lesson3
         public int X;
         public int Y;
 
+
         public static float PointDistance(PointStruct pointOne, PointStruct pointTwo)
         {
             float x = pointOne.X - pointTwo.X;
             float y = pointOne.Y - pointTwo.Y;
             return MathF.Sqrt((x * x) + (y * y));
         }
+
+        public static double PointDistanceDouble(PointStruct pointOne, PointStruct pointTwo)
+        {
+            double x = pointOne.X - pointTwo.X;
+            double y = pointOne.Y - pointTwo.Y;
+            return Math.Sqrt((x * x) + (y * y));
+        }
+
+        public static float PointDistanceShort(PointStruct pointOne, PointStruct pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return (x * x) + (y * y);
+        }
+
     }
 }
--- Lesson3/Lesson3/Program.cs
+++ Lesson3/Lesson3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace Lesson3
 {
@@ -6,7 +8,58 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
+
+    public class BechmarkClass
+    {
+
+        [Benchmark]
+        public void TestPointClass()
+        {
+            PointClass PointNew = new PointClass();
+            PointNew.X = 2;
+            PointNew.Y = 4;
+            PointClass PointNew2 = new PointClass();
+            PointNew2.X = 12;
+            PointNew2.Y = 13;
+            PointClass.PointDistance(PointNew, PointNew2);
+        }
+
+        [Benchmark]
+        public void TestPointStruct()
+        {
+            PointStruct PointNew = new PointStruct();
+            PointNew.X = 2;
+            PointNew.Y = 4;
+            PointStruct PointNew2 = new PointStruct();
+            PointNew2.X = 12;
+            PointNew2.Y = 13;
+            PointStruct.PointDistance(PointNew, PointNew2);
+        }
+        [Benchmark]
+        public void TestPointStructDouble()
+        {
+            PointStruct PointNew = new PointStruct();
+            PointNew.X = 2;
+            PointNew.Y = 4;
+            PointStruct PointNew2 = new PointStruct();
+            PointNew2.X = 12;
+            PointNew2.Y = 13;
+            PointStruct.PointDistanceDouble(PointNew, PointNew2);
+        }
+        [Benchmark]
+        public void TestPointStructShort()
+        {
+            PointStruct PointNew = new PointStruct();
+            PointNew.X = 2;
+            PointNew.Y = 4;
+            PointStruct PointNew2 = new PointStruct();
+            PointNew2.X = 12;
+            PointNew2.Y = 13;
+            PointStruct.PointDistanceShort(PointNew, PointNew2);
+        }
+    }
+
 }
