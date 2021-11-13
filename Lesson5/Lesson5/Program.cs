906e83a871ef4e939a88a40a796f722b21ee3c5d
GeekBrains <kubasovae@mail.ru>
2021-11-13 23:29:46 +0400

    Array

---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rmd = new Random();
+            int SrtCnt = rmd.Next(0,10000);
+            string[] array = new string[SrtCnt];
+            for (int i = 0; i < SrtCnt; i++) {
+                var rString = "";
+                for (var j = 0; j < 5; j++) rString += ((char)(rmd.Next(1, 26) + 64)).ToString().ToLower();
+                array[i] = rString;
+            }
+        }
+    }
+}
