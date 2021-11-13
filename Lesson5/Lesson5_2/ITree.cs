d0fb5762a782a33daad5cf4395bdab73c354b55d
GeekBrains <kubasovae@mail.ru>
2021-11-14 00:59:22 +0400

    class tree

---
+++ Lesson5/Lesson5_2/ITree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_2
+{
+    interface ITree
+    {
+        TreeNode GetRoot();
+        void AddItem(int value); // добавить узел
+        void RemoveItem(int value); // удалить узел по значению
+        TreeNode GetNodeByValue(int value); //получить узел дерева по значению
+        void PrintTree(); //вывести дерево в консоль
+    }
+}
