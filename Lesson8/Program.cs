fff34c387eed062ddcce60df629888a3305c8beb
Elena <kubasovae@mail.ru>
2021-11-26 12:58:23 +0400

    BucketSort

---
+++ Lesson8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[] { -1, 0, -10, 8, -1, 0, 78 };
+            BucketSort.Sort(ref data);
+            foreach (int value in data.Reverse())
+            {
+                Console.Write(" " + value);
+            }
+        }
+    }
+}
